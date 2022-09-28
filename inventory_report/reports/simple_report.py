cb451a877c2e4cc9e4fa099161dbd4db7adc184d
thales-bpl <thales.bplima@gmail.com>
2022-09-28 11:48:26 -0300

    tryfix: return of SimpleReport.generate()

--- inventory_report/reports/simple_report.py
+++ inventory_report/reports/simple_report.py
@@ -2,22 +2,19 @@ class SimpleReport:
     @classmethod
     def generate(cls, productList: list):
         return (
-            f"Data de fabricação mais antiga: "
-            f"{cls.get_older_manufactored(productList)}\n"
-            f"Data de validade mais próxima: "
-            f"{cls.get_nearest_expiration(productList)}\n"
-            f"Empresa com mais produtos: "
-            f"{cls.get_company_most_products(productList)}"
+            f"Data de fabricação mais antiga: {cls.oldest_date(productList)}\n"
+            f"Data de validade mais próxima: {cls.closest_date(productList)}\n"
+            f"Empresa com mais produtos: {cls.cmpny_bigger_stock(productList)}"
         )
 
-    def get_older_manufactored(cls, productList: list):
+    def oldest_date(cls, productList: list):
         manufacturing_date = [row["data_de_fabricacao"] for row in productList]
         return min(manufacturing_date)
 
-    def get_nearest_expiration(cls, productList: list):
+    def closest_date(cls, productList: list):
         expire_dates = [row["data_de_validade"] for row in productList]
         return min(expire_dates)
 
-    def get_company_most_products(cls, productList: list):
+    def cmpny_bigger_stock(cls, productList: list):
         companies_list = [row["nome_da_empresa"] for row in productList]
         return max(set(companies_list), key=companies_list.count)
