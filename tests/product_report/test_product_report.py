ba6ab16ab68ffb7e75713b89d967188cadb933d9
thales-bpl <thales.bplima@gmail.com>
2022-09-28 04:36:03 -0300

    test: class Product.__repr__()

--- tests/product_report/test_product_report.py
+++ tests/product_report/test_product_report.py
@@ -1,5 +1,19 @@
-# from inventory_report.inventory.product import Product
+from inventory_report.inventory.product import Product
 
 
 def test_relatorio_produto():
-    pass  # Seu teste deve ser escrito aqui
+    product = Product(
+        1,
+        "Nicotine Polacrilex",
+        "Target Corporation",
+        "2021-02-18",
+        "2023-09-17",
+        "CR25 1551 4467 2549 4402 1",
+        "instrucao 1"
+    )
+
+    assert product.__repr__() == "O produto Nicotine Polacrilex"\
+        " fabricado em 2021-02-18"\
+        " por Target Corporation com validade"\
+        " até 2023-09-17"\
+        " precisa ser armazenado instrucao 1."
