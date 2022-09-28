c394186bc6d38bf90895851906ea87727e558d9b
thales-bpl <thales.bplima@gmail.com>
2022-09-28 04:19:30 -0300

    test: class Product

--- tests/product/test_product.py
+++ tests/product/test_product.py
@@ -1,5 +1,21 @@
-# from inventory_report.inventory.product import Product
+from inventory_report.inventory.product import Product
 
 
 def test_cria_produto():
-    pass  # Seu teste deve ser escrito aqui
+    product = Product(
+        1,
+        "Nicotine Polacrilex",
+        "Target Corporation",
+        "2021-02-18",
+        "2023-09-17",
+        "CR25 1551 4467 2549 4402 1",
+        "instrucao 1"
+    )
+
+    assert product.id == 1
+    assert product.nome_do_produto == "Nicotine Polacrilex"
+    assert product.nome_da_empresa == "Target Corporation"
+    assert product.data_de_fabricacao == "2021-02-18"
+    assert product.data_de_validade == "2023-09-17"
+    assert product.numero_de_serie == "CR25 1551 4467 2549 4402 1"
+    assert product.instrucoes_de_armazenamento == "instrucao 1"
