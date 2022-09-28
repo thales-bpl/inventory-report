25c614f4619d29ea573563b28b8ed636f1eb5757
thales-bpl <thales.bplima@gmail.com>
2022-09-28 12:12:47 -0300

    fix: SimpleReport.generate() return

--- inventory_report/reports/simple_report.py
+++ inventory_report/reports/simple_report.py
@@ -1,20 +1,23 @@
 class SimpleReport:
     @classmethod
-    def generate(cls, productList: list):
+    def generate(self, productList: list):
         return (
-            f"Data de fabricação mais antiga: {cls.oldest_date(productList)}\n"
-            f"Data de validade mais próxima: {cls.closest_date(productList)}\n"
-            f"Empresa com mais produtos: {cls.cmpny_bigger_stock(productList)}"
+            f"Data de fabricação mais antiga: "
+            f"{self.oldest_date(productList)}\n"
+            f"Data de validade mais próxima: "
+            f"{self.closest_date(productList)}\n"
+            f"Empresa com mais produtos: "
+            f"{self.cmpny_bigger_stock(productList)}"
         )
 
-    def oldest_date(cls, productList: list):
+    def oldest_date(productList):
         manufacturing_date = [row["data_de_fabricacao"] for row in productList]
         return min(manufacturing_date)
 
-    def closest_date(cls, productList: list):
+    def closest_date(productList):
         expire_dates = [row["data_de_validade"] for row in productList]
         return min(expire_dates)
 
-    def cmpny_bigger_stock(cls, productList: list):
+    def cmpny_bigger_stock(productList):
         companies_list = [row["nome_da_empresa"] for row in productList]
         return max(set(companies_list), key=companies_list.count)
